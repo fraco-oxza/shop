aff4898151e52d0c06dccdb60c6168a0f210f7bf
Francisco Carvajal <fraco_oxza@outlook.com>
2021-09-20 16:05:18 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+// external imports
+import Head from "next/head";
+
+// internal imports
+
+// Styles
+
+// Resources
+
+function Index() {
+  return (
+    <>
+      <Head>
+        <title>Hola Mundo</title>
+      </Head>
+      <span>Hola</span>
+    </>
+  );
+}
+
+export default Index;
